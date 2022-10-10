86e8e2aefe9a509e5cc52dab1311b8c22773823b
langel <knoxburry@gmail.com>
2022-10-09 21:53:50 -0400

    refactored stuff out of main

---
+++ game/src/frame_draw.c
@@ -0,0 +1,79 @@
+
+// Update and draw game frame
+static void UpdateDrawFrame(void)
+{
+    // Update
+    //----------------------------------------------------------------------------------
+    UpdateMusicStream(music);       // NOTE: Music keeps playing between screens
+
+    if (!onTransition)
+    {
+        switch(currentScreen)
+        {
+            case LOGO:
+            {
+                UpdateLogoScreen();
+
+                if (FinishLogoScreen()) TransitionToScreen(TITLE);
+
+            } break;
+            case TITLE:
+            {
+                UpdateTitleScreen();
+
+                if (FinishTitleScreen() == 1) TransitionToScreen(OPTIONS);
+                else if (FinishTitleScreen() == 2) TransitionToScreen(GAMEPLAY);
+
+            } break;
+            case OPTIONS:
+            {
+                UpdateOptionsScreen();
+
+                if (FinishOptionsScreen()) TransitionToScreen(TITLE);
+
+            } break;
+            case GAMEPLAY:
+            {
+                UpdateGameplayScreen();
+
+                if (FinishGameplayScreen() == 1) TransitionToScreen(ENDING);
+                //else if (FinishGameplayScreen() == 2) TransitionToScreen(TITLE);
+
+            } break;
+            case ENDING:
+            {
+                UpdateEndingScreen();
+
+                if (FinishEndingScreen() == 1) TransitionToScreen(TITLE);
+
+            } break;
+            default: break;
+        }
+    }
+    else UpdateTransition();    // Update transition (fade-in, fade-out)
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        switch(currentScreen)
+        {
+            case LOGO: DrawLogoScreen(); break;
+            case TITLE: DrawTitleScreen(); break;
+            case OPTIONS: DrawOptionsScreen(); break;
+            case GAMEPLAY: DrawGameplayScreen(); break;
+            case ENDING: DrawEndingScreen(); break;
+            default: break;
+        }
+
+        // Draw full screen rectangle in front of everything
+        if (onTransition) DrawTransition();
+
+        DrawFPS(10, 10);
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+}
